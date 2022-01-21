fa0b89780d9ca8d9edc08bdba31719b235ed77b5
AkiraW5 <p16195370@gmail.com>
2022-01-21 21:30:51 +0000

    Co-authored-by: kreb <krebox@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("test")
+# Made by Kitten Team in Brazil :)
+
+from ast import Store
+from operator import rshift
+
+a = "| Howdy Folks! Welcome to LDU! |"
+print(len(a))
+print(a.center(300))
+
+print("================================")
+print("\n            Now, what you wanna do?")
+print("\n 1- Show the store ", "2- Want to suggest us something?",
+      "3- Custom downloader(BETA)")
+print("\n 4- Look our Github page!", "5- What's LDU?", "6- Let me out! (quit)")
+
+Option_Selector = input("")
+
+print(Option_Selector == True)
+
+if Option_Selector == "1" == True:
+    print("====================================================")
+    print("Application", "Arch", "Version", "Repository", "Size")
+    print("====================================================")
+
+elif Option_Selector == "2" == True:
+    print("==================================")
+    print("| Oh, so you have an suggestion? |")
+    print("==================================")
