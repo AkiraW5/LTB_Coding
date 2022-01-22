59eb306ba5748c6e02f4f3d25ee540b32b92aabf
AkiraW5 <p16195370@gmail.com>
2022-01-22 00:52:38 +0000

    Fixing Text Formatting

--- main.py
+++ main.py
@@ -14,53 +14,58 @@ Back: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
 Style: DIM, NORMAL, BRIGHT, RESET_ALL
 '''
 
-print(f"{Fore.BLUE}Red Text")
-print(Fore.RED + 'test')
-print(Back.CYAN + 'test')
-print(Style.BRIGHT + 'test')
-print(Fore.RED + Back.GREEN + 'red text on green background')
+C = Fore.BLUE + Back.BLACK
+C1 = Fore.GREEN + Back.BLACK
 
 width = os.get_terminal_size().columns
 print("\033[0;0m Bright Green  \n")
 
-print("================================".center(width))
-print("| Howdy Folks! Welcome to LTB! |".center(width))
-print("================================".center(width))
-print("Now, what you wanna do?".center(width))
-print("1- Let's use LTB!".center(width))
-print("3- Downloader (BETA)".center(width))
-print("4- Look our Github page!".center(width))
-print("5- What's LTB?".center(width))
-print("6- Let me out! (quit)".center(width))
+print(C + "================================".center(width))
+print(C + "| Howdy Folks! Welcome to LTB! |".center(width))
+print(C + "================================".center(width))
+print(C + "Now, what you wanna do?".center(width))
+print(C + "1- Let's use LTB!".center(width))
+print(C + "3- Downloader (BETA)".center(width))
+print(C + "4- Look our Github page!".center(width))
+print(C + "5- What's LTB?".center(width))
+print(C + "6- Let me out! (quit)".center(width))
 
-slt = int(input(':'.center(width)))
+slt = int(input(Fore.YELLOW + Back.BLACK + 'Type Option: '))
 
 if slt == 1:
-    print("====================================================".center(width))
-    print("|                 LTB Options list                 |".center(width))
-    print("====================================================".center(width))
+    print(C1 +
+          "====================================================".center(width))
+    print(C1 +
+          "|                 LTB Options list                 |".center(width))
+    print(C1 +
+          "====================================================".center(width))
 
 elif slt == 2:
-    print("====================================================".center(width))
-    print("|            Oh, so you have an suggestion?        |".center(width))
-    print("====================================================".centerwidth)
-    print("Nice to hear! Sadly, Due to lack of experience, we".center(width))
-    print("can't add this now! But you can contact us in TG!".center(width))
-    print("@krebox @akirawa1")
+    print(C1 +
+          "====================================================".center(width))
+    print(C1 +
+          "|            Oh, so you have an suggestion?        |".center(width))
+    print(C1 +
+          "====================================================".center(width))
+    print(C1 +
+          "Nice to hear! Sadly, Due to lack of experience, we".center(width))
+    print(C1 +
+          "can't add this now! But you can contact us in TG!".center(width))
+    print(C1 + "@krebox @akirawa1")
     print("")
 
 elif slt == 3:
-    print("====================================================")
-    print("|           Not avaliable yet! We're Sorry!        |")
-    print("====================================================")
+    print(C1 + "====================================================")
+    print(C1 + "|           Not avaliable yet! We're Sorry!        |")
+    print(C1 + "====================================================")
     print("")
 
 elif slt == 4:
-    print("====================================================")
-    print("|            Hmm... Where did you get this?        |")
-    print("====================================================")
-    print("| Anyways, there you go! |")
-    print("| https://github.com/AkiraW5/LTB_Coding |")
+    print(C1 + "====================================================")
+    print(C1 + "|            Hmm... Where did you get this?        |")
+    print(C1 + "====================================================")
+    print(C1 + "| Anyways, there you go! |")
+    print(C1 + "| https://github.com/AkiraW5/LTB_Coding |")
 
 elif slt == 6:
     quit
