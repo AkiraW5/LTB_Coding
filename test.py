b96a6aa49b8d2fcf69cedec477637bd326ef16bc
AkiraW5 <p16195370@gmail.com>
2022-01-22 20:59:36 +0000

    testing options

--- test.py
+++ test.py
@@ -18,22 +18,6 @@ Back: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
 Style: DIM, NORMAL, BRIGHT, RESET_ALL
 '''
 
-RELEASE_DATA = {}
-with open("/etc/os-release") as f:
-    reader = csv.reader(f, delimiter="=")
-    for row in reader:
-        if row:
-            RELEASE_DATA[row[0]] = row[1]
-if RELEASE_DATA["ID"] in ["debian", "raspbian"]:
-    with open("/etc/debian_version") as f:
-        DEBIAN_VERSION = f.readline().strip()
-    major_version = DEBIAN_VERSION.split(".")[0]
-    version_split = RELEASE_DATA["VERSION"].split(" ", maxsplit=1)
-    if version_split[0] == major_version:
-        # Just major version shown, replace it with the full version
-        RELEASE_DATA["VERSION"] = " ".join([DEBIAN_VERSION] +
-                                           version_split[1:])
-
 C = Fore.BLUE + Back.BLACK
 C1 = Fore.GREEN + Back.BLACK
 
@@ -45,9 +29,7 @@ machine = platform.machine()
 GetUser = getpass.getuser()
 UserInf = Back.BLACK + 'LINUX TOOLBOX' + Fore.BLUE + ' 0.1 ' + Fore.WHITE + ' | ' + Fore.BLUE + machine + Fore.WHITE + ' | ' + 'USER: ' + Fore.YELLOW + GetUser.upper(
 ) + Fore.WHITE + ' | '
-UserInf2 = Back.BLACK + 'YOUR CURRENT SO: ' + Fore.GREEN + RELEASE_DATA[
-    "NAME"] + Fore.WHITE + ' | ' + 'VERSION: ' + Fore.GREEN + RELEASE_DATA[
-        "VERSION"] + Fore.WHITE + ' | '
+UserInf2 = Back.BLACK + 'YOUR CURRENT SO: ' + Fore.GREEN + release + Fore.WHITE + ' | ' + 'VERSION: ' + Fore.GREEN + version + Fore.WHITE + ' | '
 
 opcao = 0
 while opcao != 6:
