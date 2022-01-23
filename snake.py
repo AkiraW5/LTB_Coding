f5d9f5cf382e616c7b70783fec743d6bf141ec40
AkiraW5 <p16195370@gmail.com>
2022-01-23 04:10:32 +0000

    snake

--- snake.py
+++ snake.py
@@ -2,28 +2,25 @@ import os
 
 os.system("pip install pygame")
 
-import pygame
-import sys
-import time
-import random
+import pygame, sys, time, random
 
 speed = 15
 
 #windows sizes
 
-frame_size_x = 720
-frame_size_y = 480
+frame_size_x = 1380
+frame_size_y = 840
 
 check_errors = pygame.init()
 
 if (check_errors[1] > 0):
-    print("Error", check_errors[1])
+    print("Error ", check_errors[1])
 else:
-    print("Game succesfully Initialiazed!")
+    print("Game Succesfully initialized")
 
-#initialise game windows
+#initialise game window
 
-pygame.display.set_caption('Snake Game')
+pygame.display.set_caption("Snake Game")
 game_window = pygame.display.set_mode((frame_size_x, frame_size_y))
 
 # colors
@@ -34,8 +31,8 @@ green = pygame.Color(0, 255, 0)
 blue = pygame.Color(0, 0, 255)
 
 fps_controller = pygame.time.Clock()
-# one snack square size
-square_size = 20
+# one snake square size
+square_size = 60
 
 
 def init_vars():
@@ -47,7 +44,6 @@ def init_vars():
         random.randrange(1, (frame_size_x // square_size)) * square_size,
         random.randrange(1, (frame_size_y // square_size)) * square_size
     ]
-
     food_spawn = True
     score = 0
 
@@ -67,7 +63,7 @@ def show_score(choice, color, font, size):
     game_window.blit(score_surface, score_rect)
 
 
-#Game loop
+#game loop
 
 while True:
     for event in pygame.event.get():
@@ -84,8 +80,8 @@ while True:
             elif (event.key == pygame.K_LEFT
                   or event.key == ord("a") and direction != "RIGHT"):
                 direction = "LEFT"
-            elif (event.key == pygame.K_UP
-                  or event.key == ord("D") and direction != "LEFT"):
+            elif (event.key == pygame.K_RIGHT
+                  or event.key == ord("d") and direction != "LEFT"):
                 direction = "RIGHT"
 
     if direction == "UP":
@@ -99,11 +95,10 @@ while True:
 
     if head_pos[0] < 0:
         head_pos[0] = frame_size_x - square_size
-    elif head_pos[0] > 0:
-        head_pos[0] > frame_size_x - square_size
+    elif head_pos[0] > frame_size_x - square_size:
         head_pos[0] = 0
     elif head_pos[1] < 0:
-        head_pos[1] = frame_size_x - score
+        head_pos[1] = frame_size_y - square_size
     elif head_pos[1] > frame_size_y - square_size:
         head_pos[1] = 0
 
@@ -115,24 +110,27 @@ while True:
     else:
         snake_body.pop()
 
-    #spawn food
+    # spawn food
     if not food_spawn:
         food_pos = [
-            random.randrange(1, (frame_size_x // frame_size_y)) * square_size,
+            random.randrange(1, (frame_size_x // square_size)) * square_size,
             random.randrange(1, (frame_size_y // square_size)) * square_size
         ]
+        food_spawn = True
+
     # GFX
     game_window.fill(black)
     for pos in snake_body:
         pygame.draw.rect(
             game_window, green,
-            pygame.Rect(pos[0] + 2, pos[1] + 2, square_size - 2, square_size))
+            pygame.Rect(pos[0] + 2, pos[1] + 2, square_size - 2,
+                        square_size - 2))
 
     pygame.draw.rect(
         game_window, red,
         pygame.Rect(food_pos[0], food_pos[1], square_size, square_size))
 
-    # gamer over conditions
+    # game over condiditons
 
     for block in snake_body[1:]:
         if head_pos[0] == block[0] and head_pos[1] == block[1]:
