44309c01af2c618dcb7d5895c75da7c2768d3974
AkiraW5 <p16195370@gmail.com>
2022-01-23 06:23:59 +0000

    Co-authored-by: VitorCollxtion <VitorCollxtion@users.noreply.github.com> Co-authored-by: kreb <krebox@users.noreply.github.com>

--- main.py
+++ main.py
@@ -52,6 +52,8 @@ def Use_ltb(option):
     print(C1 +
           "====================================================".center(width))
 
+    print("nao tem nada".center(width))
+
     print(Back.BLACK + '-' * width)
     print(Back.BLACK + Fore.RED + '0- ' + Fore.GREEN + 'Back to menu')
     print(Back.BLACK + '-' * width)
@@ -60,18 +62,17 @@ def Use_ltb(option):
 
     while option1 != '0':
 
-        if opcao1 == '0':
-            menu(opcao1)
-
-        elif opcao1 < '0':
+        if option1 == '0':
+            menu(option1)
+        elif option1 < '0':
             subprocess.run('clear')
-            txt = 'ERROR: YOU NEED SELECT SOME OPTION'
+            txt = 'ERROR: YOU NEED SELECT AN OPTION'
             print(Back.BLACK + Fore.RED + '=' * width)
             print(Back.BLACK + Fore.RED + txt.center(width))
             print(Back.BLACK + Fore.RED + '=' * width)
             ctn = input(Fore.YELLOW + Back.BLACK +
                         'Type anything to continue: ')
-            Use_ltb(opcao1)
+            Use_ltb(option1)
 
         else:
             subprocess.run('clear')
@@ -81,7 +82,7 @@ def Use_ltb(option):
             print(Back.BLACK + Fore.RED + '=' * width)
             ctn = input(Fore.YELLOW + Back.BLACK +
                         'Type anything to continue: ')
-            Use_ltb(opcao1)
+            Use_ltb(option1)
 
         break
 
@@ -116,7 +117,7 @@ def Suggest(option):
 
         elif option1 < '0':
             subprocess.run('clear')
-            txt = 'ERROR: YOU NEED SELECT SOME OPTION'
+            txt = 'ERROR: YOU NEED SELECT AN OPTION'
             print(Back.BLACK + Fore.RED + '=' * width)
             print(Back.BLACK + Fore.RED + txt.center(width))
             print(Back.BLACK + Fore.RED + '=' * width)
@@ -162,7 +163,7 @@ def Downloader(option):
 
         elif option1 < '0':
             subprocess.run('clear')
-            txt = 'ERROR: YOU NEED SELECT SOME OPTION'
+            txt = 'ERROR: YOU NEED SELECT AN OPTION'
             print(Back.BLACK + Fore.RED + '=' * width)
             print(Back.BLACK + Fore.RED + txt.center(width))
             print(Back.BLACK + Fore.RED + '=' * width)
@@ -210,7 +211,7 @@ def GitHub_Page(option):
 
         elif option1 < '0':
             subprocess.run('clear')
-            txt = 'ERROR: YOU NEED SELECT SOME OPTION'
+            txt = 'ERROR: YOU NEED SELECT AN OPTION'
             print(Back.BLACK + Fore.RED + '=' * width)
             print(Back.BLACK + Fore.RED + txt.center(width))
             print(Back.BLACK + Fore.RED + '=' * width)
@@ -245,7 +246,7 @@ def size_box(msg):
 
 
 def menu(option):
-    while option != '7':
+    while option != '6':
         subprocess.run('clear')
         print(Back.BLACK + Fore.WHITE + '=' * width)
         print(UserInf.ljust(width))
@@ -281,147 +282,6 @@ def menu(option):
 
         elif option == '6':
             quit
-
-        elif option == '7':
-
-            speed = 15
-
-            #windows sizes
-
-            frame_size_x = 1380
-            frame_size_y = 840
-
-            check_errors = pygame.init()
-
-            if (check_errors[1] > 0):
-                print("Error ", check_errors[1])
-            else:
-                print("Game Succesfully initialized")
-
-            #initialise game window
-
-            pygame.display.set_caption("Snake Game")
-            game_window = pygame.display.set_mode((frame_size_x, frame_size_y))
-
-            # colors
-            black = pygame.Color(0, 0, 0)
-            white = pygame.Color(255, 255, 255)
-            red = pygame.Color(255, 0, 0)
-            green = pygame.Color(0, 255, 0)
-            blue = pygame.Color(0, 0, 255)
-
-            fps_controller = pygame.time.Clock()
-            # one snake square size
-            square_size = 60
-
-            def init_vars():
-                global head_pos, snake_body, food_pos, food_spawn, score, direction
-                direction = "RIGHT"
-                head_pos = [120, 60]
-                snake_body = [[120, 60]]
-                food_pos = [
-                    random.randrange(1, (frame_size_x // square_size)) *
-                    square_size,
-                    random.randrange(1, (frame_size_y // square_size)) *
-                    square_size
-                ]
-                food_spawn = True
-                score = 0
-
-            init_vars()
-
-            def show_score(choice, color, font, size):
-                score_font = pygame.font.SysFont(font, size)
-                score_surface = score_font.render("Score: " + str(score), True,
-                                                  color)
-                score_rect = score_surface.get_rect()
-                if choice == 1:
-                    score_rect.midtop = (frame_size_x / 10, 15)
-                else:
-                    score_rect.midtop = (frame_size_x / 2, frame_size_y / 1.25)
-
-                game_window.blit(score_surface, score_rect)
-
-            #game loop
-
-            while True:
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        pygame.quit()
-                        sys.exit()
-                    elif event.type == pygame.KEYDOWN:
-                        if (event.key == pygame.K_UP or event.key == ord("w")
-                                and direction != "DOWN"):
-                            direction = "UP"
-                        elif (event.key == pygame.K_DOWN
-                              or event.key == ord("s") and direction != "UP"):
-                            direction = "DOWN"
-                        elif (event.key == pygame.K_LEFT or
-                              event.key == ord("a") and direction != "RIGHT"):
-                            direction = "LEFT"
-                        elif (event.key == pygame.K_RIGHT or
-                              event.key == ord("d") and direction != "LEFT"):
-                            direction = "RIGHT"
-
-                if direction == "UP":
-                    head_pos[1] -= square_size
-                elif direction == "DOWN":
-                    head_pos[1] += square_size
-                elif direction == "LEFT":
-                    head_pos[0] -= square_size
-                else:
-                    head_pos[0] += square_size
-
-                if head_pos[0] < 0:
-                    head_pos[0] = frame_size_x - square_size
-                elif head_pos[0] > frame_size_x - square_size:
-                    head_pos[0] = 0
-                elif head_pos[1] < 0:
-                    head_pos[1] = frame_size_y - square_size
-                elif head_pos[1] > frame_size_y - square_size:
-                    head_pos[1] = 0
-
-                #eating apple
-                snake_body.insert(0, list(head_pos))
-                if head_pos[0] == food_pos[0] and head_pos[1] == food_pos[1]:
-                    score += 1
-                    food_spawn = False
-                else:
-                    snake_body.pop()
-
-                # spawn food
-                if not food_spawn:
-                    food_pos = [
-                        random.randrange(1, (frame_size_x // square_size)) *
-                        square_size,
-                        random.randrange(1, (frame_size_y // square_size)) *
-                        square_size
-                    ]
-                    food_spawn = True
-
-                # GFX
-                game_window.fill(black)
-                for pos in snake_body:
-                    pygame.draw.rect(
-                        game_window, green,
-                        pygame.Rect(pos[0] + 2, pos[1] + 2, square_size - 2,
-                                    square_size - 2))
-
-                pygame.draw.rect(
-                    game_window, red,
-                    pygame.Rect(food_pos[0], food_pos[1], square_size,
-                                square_size))
-
-                # game over condiditons
-
-                for block in snake_body[1:]:
-                    if head_pos[0] == block[0] and head_pos[1] == block[1]:
-                        init_vars()
-
-                show_score(1, white, 'consolas', 20)
-                pygame.display.update()
-                fps_controller.tick(speed)
-            break
 
         elif option < '0':
             subprocess.run('clear')
