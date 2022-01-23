c8cddffb2d808bd62efa0a68cffe158b9560d15c
AkiraW5 <p16195370@gmail.com>
2022-01-23 05:03:17 +0000

    test

--- main.py
+++ main.py
@@ -119,63 +119,81 @@ def snake():
                       or event.key == ord("d") and direction != "LEFT"):
                     direction = "RIGHT"
 
-        if direction == "UP":
-            head_pos[1] -= square_size
-        elif direction == "DOWN":
-            head_pos[1] += square_size
-        elif direction == "LEFT":
-            head_pos[0] -= square_size
-        else:
-            head_pos[0] += square_size
 
-        if head_pos[0] < 0:
-            head_pos[0] = frame_size_x - square_size
-        elif head_pos[0] > frame_size_x - square_size:
-            head_pos[0] = 0
-        elif head_pos[1] < 0:
-            head_pos[1] = frame_size_y - square_size
-        elif head_pos[1] > frame_size_y - square_size:
-            head_pos[1] = 0
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if (event.key == pygame.K_UP
+                    or event.key == ord("w") and direction != "DOWN"):
+                direction = "UP"
+            elif (event.key == pygame.K_DOWN
+                  or event.key == ord("s") and direction != "UP"):
+                direction = "DOWN"
+            elif (event.key == pygame.K_LEFT
+                  or event.key == ord("a") and direction != "RIGHT"):
+                direction = "LEFT"
+            elif (event.key == pygame.K_RIGHT
+                  or event.key == ord("d") and direction != "LEFT"):
+                direction = "RIGHT"
 
-        #eating apple
-        snake_body.insert(0, list(head_pos))
-        if head_pos[0] == food_pos[0] and head_pos[1] == food_pos[1]:
-            score += 1
-            food_spawn = False
-        else:
-            snake_body.pop()
+    if direction == "UP":
+        head_pos[1] -= square_size
+    elif direction == "DOWN":
+        head_pos[1] += square_size
+    elif direction == "LEFT":
+        head_pos[0] -= square_size
+    else:
+        head_pos[0] += square_size
 
-        # spawn food
-        if not food_spawn:
-            food_pos = [
-                random.randrange(1,
-                                 (frame_size_x // square_size)) * square_size,
-                random.randrange(1,
-                                 (frame_size_y // square_size)) * square_size
-            ]
-            food_spawn = True
+    if head_pos[0] < 0:
+        head_pos[0] = frame_size_x - square_size
+    elif head_pos[0] > frame_size_x - square_size:
+        head_pos[0] = 0
+    elif head_pos[1] < 0:
+        head_pos[1] = frame_size_y - square_size
+    elif head_pos[1] > frame_size_y - square_size:
+        head_pos[1] = 0
 
-        # GFX
-        game_window.fill(black)
-        for pos in snake_body:
-            pygame.draw.rect(
-                game_window, green,
-                pygame.Rect(pos[0] + 2, pos[1] + 2, square_size - 2,
-                            square_size - 2))
+    #eating apple
+    snake_body.insert(0, list(head_pos))
+    if head_pos[0] == food_pos[0] and head_pos[1] == food_pos[1]:
+        score += 1
+        food_spawn = False
+    else:
+        snake_body.pop()
 
+    # spawn food
+    if not food_spawn:
+        food_pos = [
+            random.randrange(1, (frame_size_x // square_size)) * square_size,
+            random.randrange(1, (frame_size_y // square_size)) * square_size
+        ]
+        food_spawn = True
+
+    # GFX
+    game_window.fill(black)
+    for pos in snake_body:
         pygame.draw.rect(
-            game_window, red,
-            pygame.Rect(food_pos[0], food_pos[1], square_size, square_size))
+            game_window, green,
+            pygame.Rect(pos[0] + 2, pos[1] + 2, square_size - 2,
+                        square_size - 2))
 
-        # game over condiditons
+    pygame.draw.rect(
+        game_window, red,
+        pygame.Rect(food_pos[0], food_pos[1], square_size, square_size))
 
-        for block in snake_body[1:]:
-            if head_pos[0] == block[0] and head_pos[1] == block[1]:
-                init_vars()
+    # game over condiditons
 
-        show_score(1, white, 'consolas', 20)
-        pygame.display.update()
-        fps_controller.tick(speed)
+    for block in snake_body[1:]:
+        if head_pos[0] == block[0] and head_pos[1] == block[1]:
+            init_vars()
+
+    show_score(1, white, 'consolas', 20)
+    pygame.display.update()
+    fps_controller.tick(speed)
 
 
 def Use_ltb(option):
