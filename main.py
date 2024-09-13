84c507428b0009ceb61776debc2f3606d05c6fb3
AkiraW5 <p16195372@gmail.com>
2024-09-13 17:27:14 -0300

    Nova forma da interface

--- main.py
+++ main.py
@@ -19,59 +19,65 @@ def clear_screen():
     """Limpa a tela dependendo do sistema operacional."""
     subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
 
-def print_centered_message(msg):
-    """Imprime uma mensagem centralizada."""
-    print(msg.center(width))
+def print_centered_message(msg, color=Fore.WHITE):
+    """Imprime uma mensagem centralizada com cor opcional."""
+    print(color + msg.center(width))
 
-def print_separator(char='=', length=None):
-    """Imprime uma linha de separação com o caractere especificado."""
-    print(char * (length or width))
+def print_separator(char='─', length=None, color=Fore.CYAN):
+    """Imprime uma linha de separação com o caractere especificado e cor."""
+    print(color + char * (length or width))
 
 def print_error_message(msg):
     """Imprime uma mensagem de erro."""
     clear_screen()
-    print(Back.BLACK + Fore.RED + '=' * width)
-    print(Back.BLACK + Fore.RED + msg.center(width))
-    print(Back.BLACK + Fore.RED + '=' * width)
+    print_separator('=', color=Fore.RED)
+    print_centered_message(msg, Fore.RED)
+    print_separator('=', color=Fore.RED)
 
 def print_message(title, lines, footer):
     """Imprime uma mensagem com título, linhas e rodapé."""
     clear_screen()
-    print_centered_message(title)
+    print_centered_message(title, Fore.CYAN)
     print_separator()
     for line in lines:
         print_centered_message(line)
     print_separator()
-    print_centered_message(footer)
+    print_centered_message(footer, Fore.YELLOW)
     input(Fore.YELLOW + Back.BLACK + 'Type option: ')
 
 # Funções de menu
-def print_menu():
-    """Imprime o menu principal."""
+def print_refined_menu():
+    """Imprime o menu principal com refinamentos."""
     clear_screen()
+    # Cabeçalho com bordas estilizadas
+    print(Fore.CYAN + "┌" + "─" * (width - 2) + "┐")
+    print_centered_message(f"LINUX TOOLBOX 0.1  | {machine} | USER: {username.upper()}", Fore.CYAN)
+    print_centered_message(f"YOUR CURRENT SO: {release} | VERSION: {version}", Fore.CYAN)
+    print(Fore.CYAN + "└" + "─" * (width - 2) + "┘")
+    
+    # Mensagem de boas-vindas
+    print_centered_message("=" + "Howdy Folks! Welcome to LTB!".center(width - 2) + "=", Fore.YELLOW)
     print_separator()
-    print_centered_message(f'LINUX TOOLBOX 0.1  | {machine} | USER: {username.upper()}')
-    print_centered_message(f'YOUR CURRENT SO: {release} | VERSION: {version}')
-    print_separator()
-    print_centered_message("=" + "Howdy Folks! Welcome to LTB!".center(width - 2) + "=")
-    print_separator()
+
+    # Opções do menu
     options = [
         "Now, what you wanna do?",
-        "1- Let's use LTB!",
-        "2- Want to suggest us something?",
-        "3- Downloader (BETA)",
-        "4- Look our Github page!",
-        "5- What's LTB?",
-        "6- Let me out! (quit)"
+        Fore.GREEN + "1- Let's use LTB!",
+        Fore.GREEN + "2- Want to suggest us something?",
+        Fore.GREEN + "3- Downloader (BETA)",
+        Fore.GREEN + "4- Look our Github page!",
+        Fore.GREEN + "5- What's LTB?",
+        Fore.RED + "6- Let me out! (quit)"
     ]
     for option in options:
         print_centered_message(option)
-    print_separator()
+    
+    print_separator(Fore.CYAN)
 
 def handle_option(option):
     """Manipula a opção selecionada pelo usuário."""
     options = {
-        '1': lambda: print_message("LTB Options list", ["nao tem nada"], '0- Back to menu'),
+        '1': lambda: print_message("LTB Options list", ["No options available"], '0- Back to menu'),
         '2': lambda: print_message("Oh, so you have a suggestion?", [
             "Nice to hear! Sadly, Due to lack of experience, we",
             "can't add this now! But you can contact us in TG!",
@@ -98,7 +104,7 @@ def handle_option(option):
 
 # Iniciar o menu
 while True:
-    print_menu()
+    print_refined_menu()
     option = input(Fore.YELLOW + Back.BLACK + 'Type option: ')
     if handle_option(option):
         continue  # Retorna ao menu principal
